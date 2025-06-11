a6f39318343628cafddb7c593e69e6434c7af499
flore <floresteban035@gmail.com>
2025-06-10 21:39:16 -0300

    Ejercicio 1 lvl 1 funciones

--- src/F_LVL1.java
+++ src/F_LVL1.java
@@ -1,9 +1,13 @@
 public class F_LVL1 {
-
     // Ejercicio 1
     public void mostrarSaludo()
     {
         String MSG = "Hola Mundo";
         System.out.println(MSG);
+    }
+
+    public  void saludar(String nombre)
+    {
+        System.out.println("Hola, " + nombre);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,29 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        F_LVL1 Ejercicio = new F_LVL1();
+
+        Scanner teclado = new Scanner(System.in);
+        String input = "";
+
+        while (true)
+        {
+            System.out.println("Selecciona el ejercicio / Ingrese 999 para salir");
+            input = teclado.next();
+
+            if (input.equals("999")) {
+                break;
+            }
+
+            if (input.equals("1")) {
+                Ejercicio.mostrarSaludo();
+            }
+
+            if (input.equals("2")) {
+                String nombre = "Esteban";
+                Ejercicio.saludar(nombre);
+            }
+        }
     }
 }
