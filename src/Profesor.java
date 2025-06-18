8acc9c931f68dfcd1e9782c849cd36d13cafcad7
flore <floresteban035@gmail.com>
2025-06-17 22:32:36 -0300

    Cree la clase profesor y cree los getters y setters

--- src/Profesor.java
+++ src/Profesor.java
@@ -6,7 +6,26 @@ public class Profesor extends Persona {
     public Profesor(String nombre, String apellido, String numId, int edad, String direccion, String telefono,
                     String departamento, double salario, int añosDeExperiencia) {
         super(nombre, apellido, numId, edad, direccion, telefono);
-
+        setDepartamento(departamento);
+        setSalario(salario);
+        setAñosDeExperiencia(añosDeExperiencia);
     }
 
+    public String getDepartamento() {return departamento;}
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public double getSalario() {return salario;}
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getAñosDeExperiencia() {return añosDeExperiencia;}
+
+    public void setAñosDeExperiencia(int añosDeExperiencia) {
+        this.añosDeExperiencia = añosDeExperiencia;
+    }
 }
